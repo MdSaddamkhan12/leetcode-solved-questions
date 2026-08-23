5ef3163a1f0b034818504bb330f0de25dc92231f
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2026-08-23 11:00:20 +0530

    Time: 0 ms (100%), Space: 42.5 MB (93.61%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,61 +1,83 @@
 class MyQueue {
 
-    Stack<Integer> st1;
-    Stack<Integer> st2;
+    // for storing newly pushed elements
+    Stack<Integer> stackIn;
+
+    //for providing elements for pop() and peek().
+    // The top of stackOut always represents the FRONT of the queue.
+    Stack<Integer> stackOut;
 
     public MyQueue() {
 
-        st1 = new Stack<Integer>();
-        st2 = new Stack<Integer>();
+        stackIn = new Stack<Integer>();
+        stackOut = new Stack<Integer>();
         
     }
-    
+    /**
+     * Adds an element to the end of the queue.
+     *
+     * We simply push the element into stackIn.
+     * We don't move elements to stackOut here because insertion
+     * can be handled directly by stackIn.
+     */
     public void push(int x) {
 
-        // copy all elements from st1 -> st2
-        while(!st1.isEmpty()){
-
-            st2.push(st1.pop());
-        }
-
-        // push new element to st2
-        st2.push(x);
-
-        // again copy all elements from st2 -> st1
-        while(!st2.isEmpty()){
-
-            st1.push(st2.pop());
-        }
+        stackIn.push(x);
         
     }
     
+    /**
+     * Removes and returns the element at the front of the queue.
+     *
+     * If stackOut is empty, transfer all elements from stackIn
+     * to stackOut. This reverses their order and makes the
+     * oldest element available at the top of stackOut.
+     */
     public int pop() {
 
-        // if(st1.isEmpty()){
-
-        //     Sysyem.out.println("Queue is empty.");
-        //     return -1;
-        // }
-        return st1.pop();
+        moveIfNeeded();
+        return stackOut.pop();
         
     }
 
-    
+    /**
+     * Returns the element at the front of the queue
+     * without removing it.
+     *
+     * Make sure stackOut contains the front element first.
+     */
     public int peek() {
 
-        
-        // if(st1.isEmpty()){
-
-        //     Sysyem.out.println("Queue is empty.");
-        //     return -1;
-        // }
-        return st1.peek();
+        moveIfNeeded();
+        return stackOut.peek();
         
     }
     
+    /**
+     * Returns true when both stacks are empty.
+     *
+     * If either stack contains an element, the queue is not empty.
+     */
     public boolean empty() {
         
-        return st1.isEmpty();
+        return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+
+    /**
+     * Transfers elements from stackIn to stackOut only when
+     * stackOut is empty.
+    **/
+    private void moveIfNeeded(){
+
+        if(stackOut.isEmpty()){
+
+           while(!stackIn.isEmpty()){
+
+            stackOut.push(stackIn.pop());
+
+            }
+        }
+
     }
 }
 
