c036d80848b29168263061a5850b52d609be6f00
MdSaddamkhan12 <41963533+MdSaddamkhan12@users.noreply.github.com>
2026-08-30 14:34:30 +0530

    Time: 4 ms (74.07%), Space: 45.5 MB (10.8%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        int m = nums1.length;
+
+        Stack<Integer> stack = new Stack<>();
+
+        // to store the element from nums2 and its next greater element
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+
+        int[] result = new int[m];
+
+        // Traverse the nums2 array
+        for(int num : nums2){
+
+            // Pop elements smaller than current, they found their next greater
+            //<popped value, NGE(coming value)>
+            while(!stack.isEmpty() && num > stack.peek()){
+                nextGreater.put(stack.pop(), num);
+            }
+            stack.push(num);
+
+        }
+        // Build result by looking up each nums1 element in the map
+        for(int i = 0; i < m; i++){
+
+            result[i] = nextGreater.getOrDefault(nums1[i], -1);
+        }
+        return result;
+        
+    }
+}
